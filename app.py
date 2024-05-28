70ccb9281b1f55366f5f46e7259d60a244d474d3
Florent <f.beausseron@gmail.com>
2024-05-28 21:34:39 +0200

    user input to display exercise specifics

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # pylint: disable=missing-module-docstring
+import ast
 
 import duckdb
 import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-# solution_df = duckdb.sql(ANSWER_STR).df()
+#solution_df = duckdb.sql(ANSWER_STR).df()
 
 with st.sidebar:
     theme = st.selectbox(
@@ -21,35 +22,34 @@ with st.sidebar:
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user input")
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
-#
-#     if len(result.columns) != len(solution_df.columns):
-#         st.write("Some columns are missing")
-#
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a {n_lines_difference} lines difference with the solution_df"
-#         )
-#
-#
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected:")
-#     st.dataframe(solution_df)
-#
-# with tab3:
-#     st.write(ANSWER_STR)
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
+
+#   if len(result.columns) != len(solution_df.columns):
+#       st.write("Some columns are missing")
+#   try:
+#       result = result[solution_df.columns]
+#       st.dataframe(result.compare(solution_df))
+#   except KeyError as e:
+#       st.write("Some columns are missing")
+#   n_lines_difference = result.shape[0] - solution_df.shape[0]
+#   if n_lines_difference != 0:
+#       st.write(
+#           f"result has a {n_lines_difference} lines difference with the solution_df"
+#       )
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+    st.write(answer)
